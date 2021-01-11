025430c50345646cfa25919d739df1ce7940d58a
Christian Hodges <cdhodges5@gmail.com>
2021-01-10 21:49:40 -0800

    Add definitions for base in views of apps

--- whattowatch/chatApp/views.py
+++ whattowatch/chatApp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def base(request):
+    context = {
+        "title":"Homepage",
+        }
+    return render(request, "base.html", context=context)
