5a81b545db7985500d73a1668557d16c18c68fc2
Christian Hodges <cdhodges5@gmail.com>
2021-01-10 22:42:20 -0800

    Changed "title"s in the view contexts

--- whattowatch/watchApp/views.py
+++ whattowatch/watchApp/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def base(request):
     context = {
-        "title":"Homepage",
+        "title":"Watch Homepage",
         }
     return render(request, "base.html", context=context)
