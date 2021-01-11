733f959aec47cdf5aebdc36211090398b71cd14e
Christian Hodges <cdhodges5@gmail.com>
2021-01-10 22:00:53 -0800

    static was in the wrong path. Fixed and did collectstatic function

--- whattowatch/whattowatch/settings.py
+++ whattowatch/whattowatch/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+MEDIA_URL = '/media/'
