f3a507e51e1ea9667d18229c7fe3f83b6dadeb13
Christian Hodges <cdhodges5@gmail.com>
2021-01-15 10:52:33 -0800

    react code works

--- whattowatch/whattowatch/settings.py
+++ whattowatch/whattowatch/settings.py
@@ -123,8 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-MEDIA_URL = '/media/'
-STATIC_URL = '/static/'
+MEDIA_URL = '/front_end/media/'
+STATIC_URL = '/front_end/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "front_end/media")
 STATIC_ROOT = os.path.join(BASE_DIR, "front_end/static")
 
